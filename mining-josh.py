d883a29bb0831308d03c941aaae48caa47b05b2a
Joshua Young <djry1999@gmail.com>
2021-04-14 19:23:12 +1000

    Implement class attributes and payoff, copy tests file

--- mining-josh.py
+++ mining-josh.py
@@ -46,12 +46,11 @@ from numbers import Number
 
 import search
 
-
     
 def convert_to_tuple(a):
-    '''
+    """
     Convert the parameter 'a' into a nested tuple of the same shape as 'a'.
-    
+
     The parameter 'a' must be array-like. That is, its elements are indexed.
 
     Parameters
@@ -62,7 +61,7 @@ def convert_to_tuple(a):
     -------
     the conversion of 'a' into a tuple or a tuple of tuples
 
-    '''
+    """
     if isinstance(a, Number):
         return a
     if len(a)==0:
@@ -77,8 +76,8 @@ def convert_to_tuple(a):
     
     
 def convert_to_list(a):
-    '''
-    Convert the array-like parameter 'a' into a nested list of the same 
+    """
+    Convert the array-like parameter 'a' into a nested list of the same
     shape as 'a'.
 
     Parameters
@@ -89,7 +88,7 @@ def convert_to_list(a):
     -------
     the conversion of 'a' into a list or a list of lists
 
-    '''
+    """
     if isinstance(a, Number):
         return a
     if len(a)==0:
@@ -103,55 +102,53 @@ def convert_to_list(a):
         return [list(r) for r in a]    
 
 
-
-
 class Mine(search.Problem):
-    '''
-    
-    Mine represent an open mine problem defined by a grid of cells 
+    """
+
+    Mine represent an open mine problem defined by a grid of cells
     of various values. The grid is called 'underground'. It can be
     a 2D or 3D array.
-    
+
     The z direction is pointing down, the x and y directions are surface
     directions.
-    
-    An instance of a Mine is characterized by 
-    - self.underground : the ndarray that contains the values of the grid cells
-    - self.dig_tolerance : the maximum depth difference allowed between 
-                           adjacent columns 
-    
+
+    An instance of a Mine is characterized by
+    - self._underground : the ndarray that contains the values of the grid cells
+    - self.dig_tolerance : the maximum depth difference allowed between
+                           adjacent columns
+
     Other attributes:
         self.len_x, self.len_y, self.len_z : int : underground.shape
-        self.cumsum_mine : float array : cumulative sums of the columns of the 
+        self.cumsum_mine : float array : cumulative sums of the columns of the
                                          mine
-    
+
     A state has the same dimension as the surface of the mine.
     If the mine is 2D, the state is 1D.
     If the mine is 3D, the state is 2D.
-    
+
     state[loc] is zero if digging has not started at location loc.
     More generally, state[loc] is the z-index of the first cell that has
     not been dug in column loc. This number is also the number of cells that
     have been dugged in the column.
-    
+
     States must be tuple-based.
+
+    """
     
-    '''    
-    
-    def __init__(self, underground, dig_tolerance = 1):
-        '''
+    def __init__(self, underground, dig_tolerance=1):
+        """
         Constructor
-        
+
         Initialize the attributes
-        self.underground, self.dig_tolerance, self.len_x, self.len_y, self.len_z,
+        self._underground, self.dig_tolerance, self.len_x, self.len_y, self.len_z,
         self.cumsum_mine, and self.initial
-        
+
         The state self.initial is a filled with zeros.
 
         Parameters
         ----------
         underground : np.array
-            2D or 3D. Each element of the array contains 
+            2D or 3D. Each element of the array contains
             the profit value of the corresponding cell.
         dig_tolerance : int
              Mine attribute (see class header comment)
@@ -159,19 +156,23 @@ class Mine(search.Problem):
         -------
         None.
 
-        '''
+        """
         # super().__init__() # call to parent class constructor not needed
         
-        self.underground = underground 
-        # self.underground  should be considered as a 'read-only' variable!
+        self._underground = underground
+        # self._underground  should be considered as a 'read-only' variable!
         self.dig_tolerance = dig_tolerance
         assert underground.ndim in (2,3)
         
-        raise NotImplementedError
-
+        if self._underground.ndim == 2:
+            self.len_x, self.len_z = self._underground.shape
+            self.cumsum_mine = np.cumsum(self._underground, dtype=float, axis=1)
+        else:
+            self.len_x, self.len_y, self.len_z = self._underground.shape
+            self.cumsum_mine = np.cumsum(self._underground, dtype=float, axis=2)
 
     def surface_neigbhours(self, loc):
-        '''
+        """
         Return the list of neighbours of loc
 
         Parameters
@@ -185,7 +186,7 @@ class Mine(search.Problem):
         A list of tuples representing the surface coordinates of the
         neighbouring surface cells.
 
-        '''
+        """
         L=[]
         assert len(loc) in (1,2)
         if len(loc)==1:
@@ -201,7 +202,6 @@ class Mine(search.Problem):
                 if  (0 <= loc[0]+dx < self.len_x) and (0 <= loc[1]+dy < self.len_y):
                     L.append((loc[0]+dx, loc[1]+dy))
         return L
-     
     
     def actions(self, state):
         '''
@@ -223,8 +223,7 @@ class Mine(search.Problem):
         state = np.array(state)
 
         raise NotImplementedError
-                
-  
+
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must a valid actions.
@@ -233,8 +232,7 @@ class Mine(search.Problem):
         new_state = np.array(state) # Make a copy
         new_state[action] += 1
         return convert_to_tuple(new_state)
-                
-    
+
     def console_display(self):
         '''
         Display the mine on the console
@@ -245,7 +243,7 @@ class Mine(search.Problem):
 
         '''
         print('Mine of depth {}'.format(self.len_z))
-        if self.underground.ndim == 2:
+        if self._underground.ndim == 2:
             # 2D mine
             print('Plane x,z view')
         else:
@@ -255,20 +253,15 @@ class Mine(search.Problem):
         print(self.__str__())
         
     def __str__(self):
-        if self.underground.ndim == 2:
+        if self._underground.ndim == 2:
             # 2D mine
-            return str(self.underground.T)
+            return str(self._underground.T)
         else:
             # 3D mine
             # level by level representation
             return '\n'.join('level {}\n'.format(z)
-                   +str(self.underground[...,z]) for z in range(self.len_z))
-                    
-                        
-                
-            return self.underground[loc[0], loc[1],:]
-        
-    
+                   +str(self._underground[...,z]) for z in range(self.len_z))
+
     @staticmethod   
     def plot_state(state):
         if state.ndim==1:
@@ -300,42 +293,41 @@ class Mine(search.Problem):
         plt.show()
 
     def payoff(self, state):
-        '''
+        """
         Compute and return the payoff for the given state.
         That is, the sum of the values of all the digged cells.
-        
-        No loops needed in the implementation!        
-        '''
-        # convert to np.array in order to use tuple addressing
-        # state[loc]   where loc is a tuple
-        raise NotImplementedError
 
+        No loops needed in the implementation!
+        """
+        # convert to np.array in order to use tuple addressing
+        state = np.array(state)
+        # state[loc]   where loc is a tuple
+        if state.ndim == 1:
+            return np.array([np.sum(self._underground[i][:state[i]], dtype=float, axis=0) for i in range(len(state))])
+        else:
+            return np.array([[np.sum(self._underground[j][i][:state[j][i]], dtype=float, axis=0) for i in range(len(state[j]))] for j in range(len(state))])
 
     def is_dangerous(self, state):
-        '''
+        """
         Return True iff the given state breaches the dig_tolerance constraints.
-        
+
         No loops needed in the implementation!
-        '''
+        """
         # convert to np.array in order to use numpy operators
-        state = np.array(state)         
+        state = np.array(state)
 
         raise NotImplementedError
-           
 
-
-    
     # ========================  Class Mine  ==================================
-    
-    
-    
+
+
 def search_dp_dig_plan(mine):
-    '''
-    Search using Dynamic Programming the most profitable sequence of 
+    """
+    Search using Dynamic Programming the most profitable sequence of
     digging actions from the initial state of the mine.
-    
+
     Return the sequence of actions, the final state and the payoff
-    
+
 
     Parameters
     ----------
@@ -345,17 +337,15 @@ def search_dp_dig_plan(mine):
     -------
     best_payoff, best_action_list, best_final_state
 
-    '''
+    """
     raise NotImplementedError
 
 
-    
-    
 def search_bb_dig_plan(mine):
-    '''
-    Compute, using Branch and Bound, the most profitable sequence of 
+    """
+    Compute, using Branch and Bound, the most profitable sequence of
     digging actions from the initial state of the mine.
-        
+
 
     Parameters
     ----------
@@ -366,39 +356,30 @@ def search_bb_dig_plan(mine):
     -------
     best_payoff, best_action_list, best_final_state
 
-    '''
+    """
     
     raise NotImplementedError
 
 
-
 def find_action_sequence(s0, s1):
-    '''
+    """
     Compute a sequence of actions to go from state s0 to state s1.
     There may be several possible sequences.
-    
-    Preconditions: 
-        s0 and s1 are legal states, s0<=s1 and 
-    
+
+    Preconditions:
+        s0 and s1 are legal states, s0<=s1 and
+
     Parameters
     ----------
     s0 : tuple based mine state
-    s1 : tuple based mine state 
+    s1 : tuple based mine state
 
     Returns
     -------
     A sequence of actions to go from state s0 to state s1
 
-    '''    
+    """
     # approach: among all columns for which s0 < s1, pick the column loc
     # with the smallest s0[loc]
     raise NotImplementedError
-        
-        
-        
-        
-        
-        
-    
-    
-    
+
